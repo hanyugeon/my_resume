173d46bfd5f3e0937675fc8cac31bdf452d6adc5
hanyugeon <hanyugeon@naver.com>
2023-07-05 02:23:43 +0900

    settings: 프로젝트 초기설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      azure: "#678EDC",
+      lavender: "#8C34E4",
+      mint: "#41E8D3",
+      white: "#FFFFFF",
+      gray: "#374151",
+      black: "#0A0A0A",
     },
   },
   plugins: [],
-}
+};
