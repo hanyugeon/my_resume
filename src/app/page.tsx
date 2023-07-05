c20f87437e95779330d8b8e314110505e0269711
hanyugeon <hanyugeon@naver.com>
2023-07-05 12:34:15 +0900

    feat: Button 컴포넌트 유니온타입 정의

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,11 @@ import Button from "@/components/Button";
 
 export default function Home() {
   return (
-    <main>
-      <article>
-        <Button
-          text="button"
-          linkType="github"
-          href="https://github.com/hanyugeon"
-        />
-        <Button
-          text="button"
-          linkType="link"
-          href="https://github.com/hanyugeon"
-        />
-        <Button
-          text="button"
-          linkType="web"
-          href="https://github.com/hanyugeon"
-        />
-      </article>
+    <main className="flex">
+      <Button label="button" url="https://github.com/hanyugeon" />
+      <Button label="button" icon="github" url="https://github.com/hanyugeon" />
+      <Button label="button" icon="link" url="https://github.com/hanyugeon" />
+      <Button label="button" icon="web" url="https://github.com/hanyugeon" />
     </main>
   );
 }
