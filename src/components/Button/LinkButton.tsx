32f4f64be546fe09200764b2e2fd37beab9bc779
hanyugeon <hanyugeon@naver.com>
2023-07-05 21:35:48 +0900

    chore: Button을 LinkButton으로 수정

--- src/components/Button/LinkButton.tsx
+++ src/components/Button/LinkButton.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 type IconTypes = "link" | "github" | "web";
 
-type ButtonTypes = {
+type LinkButtonProps = {
   label: string;
   icon?: IconTypes;
   url: string;
 };
 
-const Button = ({ label, icon, url }: ButtonTypes) => {
+const LinkButton = ({ label, icon, url }: LinkButtonProps) => {
   return (
     <Link href={url} target="_blank" rel={"noreferrer"}>
-      <button className="flex justify-center align-middle relative px-[1.2rem] py-[0.6rem] space-x-[0.4rem] rounded-[0.8rem] bg-azure">
+      <button className="flex justify-left align-middle px-[1.2rem] py-[0.6rem] space-x-[0.4rem] rounded-[0.8rem] bg-azure">
         {icon && (
           <Image
             src={`/ic_${icon}.svg`}
@@ -27,4 +27,4 @@ const Button = ({ label, icon, url }: ButtonTypes) => {
   );
 };
 
-export default Button;
+export default LinkButton;
