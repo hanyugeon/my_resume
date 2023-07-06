ee79016d0dfdaff126cb9a5c407c1e92fd3d28f6
hanyugeon <hanyugeon@naver.com>
2023-07-06 17:47:51 +0900

    chore: LinkButton 수정

--- src/components/Button/LinkButton.tsx
+++ src/components/Button/LinkButton.tsx
@@ -11,19 +11,19 @@ type LinkButtonProps = {
 
 const LinkButton = ({ label, icon, url }: LinkButtonProps) => {
   return (
-    <button className="flex justify-left align-middle px-[1.2rem] py-[0.6rem] space-x-[0.4rem] rounded-[0.8rem] bg-azure">
-      {icon && (
-        <Image
-          src={`/ic_${icon}.svg`}
-          width={21}
-          height={21}
-          alt="buttonIcon"
-        />
-      )}
-      <Link href={url} target="_blank" rel={"noreferrer"}>
+    <Link href={url} target="_blank" rel={"noreferrer"}>
+      <button className="flex justify-left align-middle px-[1.2rem] py-[0.6rem] space-x-[0.4rem] rounded-[0.8rem] bg-azure">
+        {icon && (
+          <Image
+            src={`/ic_${icon}.svg`}
+            width={21}
+            height={21}
+            alt="buttonIcon"
+          />
+        )}
         <span className="text-[1.4rem] text-white font-medium">{label}</span>
-      </Link>
-    </button>
+      </button>
+    </Link>
   );
 };
 
