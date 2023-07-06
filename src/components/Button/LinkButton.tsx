0a62e2dfa9b928ddc06d87e534786ff8053a8018
hanyugeon <hanyugeon@naver.com>
2023-07-06 11:41:18 +0900

    chore: 버튼 Link컴포넌트 depth변경

--- src/components/Button/LinkButton.tsx
+++ src/components/Button/LinkButton.tsx
@@ -11,19 +11,19 @@ type LinkButtonProps = {
 
 const LinkButton = ({ label, icon, url }: LinkButtonProps) => {
   return (
-    <Link href={url} target="_blank" rel={"noreferrer"}>
-      <button className="flex justify-left align-middle px-[1.2rem] py-[0.6rem] space-x-[0.4rem] rounded-[0.8rem] bg-azure">
-        {icon && (
-          <Image
-            src={`/ic_${icon}.svg`}
-            width={21}
-            height={21}
-            alt="buttonIcon"
-          />
-        )}
+    <button className="flex justify-left align-middle px-[1.2rem] py-[0.6rem] space-x-[0.4rem] rounded-[0.8rem] bg-azure">
+      {icon && (
+        <Image
+          src={`/ic_${icon}.svg`}
+          width={21}
+          height={21}
+          alt="buttonIcon"
+        />
+      )}
+      <Link href={url} target="_blank" rel={"noreferrer"}>
         <span className="text-[1.4rem] text-white font-medium">{label}</span>
-      </button>
-    </Link>
+      </Link>
+    </button>
   );
 };
 
