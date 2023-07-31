9f403fd3e0955aba723cd56754e2b82dcaecf242
hanyugeon <hanyugeon@naver.com>
2023-07-31 19:28:49 +0900

    feat: padding값에 반응형 추가

--- src/components/Experience/ArticleContainer.tsx
+++ src/components/Experience/ArticleContainer.tsx
@@ -4,7 +4,7 @@ type ChildrenType = {
 
 const ArticleContainer = ({ children }: ChildrenType) => {
   return (
-    <div className="grid gap-[0.4rem] max-w-[76.8rem] w-full px-[4rem]">
+    <div className="grid gap-[0.4rem] max-w-[76.8rem] w-full px-[1rem] sm:px-[4rem]">
       {children}
     </div>
   );
