6852d1c8056ebe5dfe13b8f943ebda46932519d8
hanyugeon <hanyugeon@naver.com>
2023-11-07 17:09:57 +0900

    feat: 반응형 추가

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -2,7 +2,7 @@ import { GradientBar } from "@/components/base";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <body className="max-w-[59.5rem] p-[4rem] mx-auto my-0 h-full">
+    <body className="max-w-[59.5rem] sm:p-[4rem] p-[2rem] mx-auto my-0 h-full">
       <header className="absolute left-0 top-0 w-full">
         <GradientBar />
       </header>
