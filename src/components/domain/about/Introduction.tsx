36b430483fb8e01409bbf41b8c20c6ba68922069
hanyugeon <hanyugeon@naver.com>
2024-02-12 17:57:32 +0900

    fix: 소개글 업데이트

--- src/components/domain/about/Introduction.tsx
+++ src/components/domain/about/Introduction.tsx
@@ -2,22 +2,22 @@ const Introduction = () => {
   return (
     <hgroup className="flex flex-col gap-[0.2rem] leading-[1.48rem] text-dark text-md">
       <p>
-        <span className="font-bold">좋은 사용자 경험을 제공합니다. </span>사용자
-        피드백을 바탕으로 좋은 UI/UX를 제공하고자 디자인 시스템을 제작한 경험이
-        있으며 낙관적 업데이트 적용 및 이미지, 폰트 최적화와 같은 페이지 성능
-        개선을 진행하였습니다.
+        <span className="font-bold">💁 좋은 사용자 경험을 제공합니다. </span>
+        사용자 친화적이고 직관적인 화면 구성을 위해 디자인 시스템을 직접 제작 및
+        도입한 경험이 있습니다. 최근에는 웹 페이지 최적화 및 UX 관련
+        레퍼런스들을 찾아보며 학습 중입니다.
       </p>
       <p>
-        <span className="font-bold">새로운 기술에 흥미가 많습니다. </span>
-        진행했던 프로젝트를 app router로 마이그레이션 및 서버 컴포넌트를
-        도입하였으며 Storybook을 활용한 CDD(컴포넌트 주도 개발)를 진행 중
-        입니다.
+        <span className="font-bold">🏄 새로운 기술에 흥미가 많습니다. </span>
+        새로운 기술을 프로젝트에 적용하기 위해 주도적인 학습을 진행했습니다.
+        새로운 버전의 Next.js와 RSC에 대한 학습 이후 진행 중인 프로젝트에 이를
+        적용하였습니다.
       </p>
       <p>
-        <span className="font-bold">커뮤니케이션을 중요하게 생각합니다. </span>
-        진행 중인 프로젝트의 주간 스크럼 진행, GitHub Issue를 활용한 티켓 관리,
-        Notion을 활용하여 문서화 하고 있으며 경험을 통해 배웠던 기술들을 개인
-        블로그에 기록 중입니다.
+        <span className="font-bold">🤝 커뮤니케이션을 중요시합니다. </span>진행
+        중인 프로젝트의 주기적인 스크럼, 코드 리뷰, 프로젝트 문서화 및 아티클
+        공유를 통해 팀원들과의 일관된 프로젝트 방향성을 유지하는 것을
+        지향합니다.
       </p>
     </hgroup>
   );
