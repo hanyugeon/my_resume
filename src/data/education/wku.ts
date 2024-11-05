87d87a964215bf9b41da9c34a7ef8a18ed9999f5
hanyugeon <hanyugeon@naver.com>
2024-11-05 18:30:31 +0900

    update: 학력 수정

--- src/data/education/wku.ts
+++ src/data/education/wku.ts
@@ -1,15 +1,17 @@
 export const wkuArticle = {
   title: "원광대학교",
-  contents: [
-    {
-      name: "컴퓨터소프트웨어공학과",
-      description: ["전과 및 졸업 (20.03 - 23.02)"],
+  contents: {
+    departmentOfComputerSoftwareEngineering: {
+      activity: {
+        status: "전과 및 졸업 (20.03 - 23.02)",
+      },
     },
-    {
-      name: "수학교육과",
-      description: ["이전학과 (17.03 - 18.02)"],
+    mathematicsEducationDepartment: {
+      activity: {
+        status: "이전학과 (17.03 - 18.02)",
+      },
     },
-  ],
+  },
 };
 
 export const wkuAside = {
