a00d009b9c7ec7874684faa0eddcc06c86f2bfe8
hanyugeon <hanyugeon@naver.com>
2024-11-05 18:32:44 +0900

    update: 활동 이력 수정

--- src/data/activity/nextjskr.ts
+++ src/data/activity/nextjskr.ts
@@ -1,14 +1,14 @@
 export const nextkrArticle = {
   title: "Next.js 공식문서 한글 번역 기여",
-  contents: [
-    {
-      name: "활동 내용",
-      description: [
-        "app/api-reference/file-conventaions/page.mdx 문서 번역",
-        "기여자들과 번역 컨벤션 및 이전 사례를 바탕으로 검수 작업 진행",
-      ],
+  contents: {
+    nextJsDocsTranslation: {
+      activity: {
+        "1": "app/api-reference/file-conventions/page.mdx 문서 번역",
+        "2": "기여자들과 컨벤션 및 이전 번역 사례를 바탕으로한 검수 작업 진행",
+      },
+      link: "https://github.com/Nextjs-kr/Nextjs.kr/pull/205",
     },
-  ],
+  },
 };
 
 export const nextkrAside = {
@@ -20,7 +20,7 @@ export const nextkrAside = {
     {
       label: "문서 원문",
       icon: "link",
-      url: "https://nextjs.org/docs/app/api-reference/file-conventions/page",
+      url: "https://nextjs.org/docs/13/app/api-reference/file-conventions/page",
     },
     {
       label: "번역 PR",
